ddc3a78e28623957cc34969ced21acad647e88b1
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-04-26 10:52:15 +0200

    feat: added accepted paths option

--- src/helpers/createHistory.ts
+++ src/helpers/createHistory.ts
@@ -1,21 +1,42 @@
 import html2canvas from "html2canvas";
+import { LocationObject, WhereWasIOptions } from "../types";
 
 const generateScreenshot = function() {
   const screenshotTarget = document.body;
   
-  return html2canvas(screenshotTarget, { scale: 0.5, logging: true }).then((canvas: HTMLCanvasElement) => {
+  return html2canvas(screenshotTarget, { scale: 0.5, logging: true,  }).then((canvas: HTMLCanvasElement) => {
     const base64image = canvas.toDataURL("image/png");
     return base64image;
   });
 }
 
 const createHistory = async function(newItem: string, history: Array<LocationObject>, options: WhereWasIOptions) {
+  if (options.acceptedPaths) {
+    let shouldReturn = false;
+    switch (options.acceptedPaths.type) {
+      case "contains":
+        shouldReturn = !newItem.includes(options.acceptedPaths.path)
+        break;
+      case "startsWith":
+        shouldReturn = !newItem.startsWith(options.acceptedPaths.path);
+        break;
+    }
+
+    if (shouldReturn) {
+      return history;
+    }
+  }
+
   const imageData = await generateScreenshot();
   const sortedHistory = [
     ...new Map([
-      { location: newItem, imageData, title: document.title, newObject: true }, 
+      { location: `${location.origin}${newItem}`, imageData, title: document.title, newObject: true }, 
       ...history.map(h => ({...h, newObject: false}))
     ].map(item => [item["location"], item])).values()];
+
+  if (options.maxAmount && sortedHistory.length > options.maxAmount) {
+    return sortedHistory.slice(0, options.maxAmount);
+  }
 
   return sortedHistory;
 }
