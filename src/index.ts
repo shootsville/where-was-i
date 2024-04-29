0ac3479162750c05425bace400e734ec46a1f620
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-04-29 11:01:51 +0200

    feat: added interval for updating screen and urlchangevent

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,62 @@
 import renderHistory from "./helpers/renderHistory";
-import createHistory from "./helpers/createHistory";
+import createHistory, { generateScreenshot } from "./helpers/createHistory";
 import applyCss from "./helpers/applyCss";
 import { LocationObject, WhereWasIOptions } from "./types";
+import 'url-change-event';
+
 export type { LocationObject, WhereWasIOptions }
 
+let INTERVAL = 0;
+
+export const getStorage = function() {
+  return JSON.parse(window.sessionStorage.getItem("wwi-items") ?? "[]") as Array<LocationObject>;
+}
+export const setStorage = function(locations: LocationObject[]) {
+  window.sessionStorage.setItem("wwi-items", JSON.stringify(locations));
+}
+
+const updateCurrentScreen = function(options: WhereWasIOptions, path: string) {
+  INTERVAL = window.setInterval(() => {
+    generateScreenshot(options).then(res => {
+      const storage = getStorage();
+      const newLocation = `${location.origin}${path}`;
+      const currentLocationObject = storage.find(s => s.location === newLocation);
+      const currentLocationElement = document.querySelector<HTMLDivElement>(`#where-was-i-container [data-location="${newLocation}"]`);
+      if (currentLocationObject && currentLocationElement) {
+        currentLocationObject.imageData = res;
+        currentLocationElement.style.background = `url(${res})`;
+      }
+
+      setStorage(storage);
+    });
+  }, options.screenRefreshRate ?? 5000);
+}
 
 const whereWasI = function(options: WhereWasIOptions = { maxAmount: 12, style: "cards" }) {
-  let storage = JSON.parse(window.sessionStorage.getItem("items") ?? "[]") as Array<LocationObject>;
+  let storage = getStorage();
+  
+  window.addEventListener("urlchangeevent", () => {
+    clearInterval(INTERVAL);
+    /** SPA:s trigger url change event before changing rendered page */
+    setTimeout(() => { 
+      createHistory(location.pathname, storage, options).then(res => {
+        storage = res;
+        setStorage(storage);
+        renderHistory(storage, options);
+      });
+      updateCurrentScreen(options, location.pathname);
+    }, 500);
 
-  window.addEventListener("navigate", () => {
-    createHistory(location.pathname, storage, options).then(res => {
-      storage = res;
-      window.sessionStorage.setItem("items", JSON.stringify(storage));
-      renderHistory(storage, options);
-    });
   });
 
   document.addEventListener("DOMContentLoaded", () => {
     createHistory(location.pathname, storage, options).then(res => {
       storage = res;
-      window.sessionStorage.setItem("items", JSON.stringify(storage));
+      setStorage(storage);
       renderHistory(storage, options)
     });
-
+    updateCurrentScreen(options, location.pathname);
+    
     applyCss(options);
   });  
 }
