8e483161e143b878d9ae3f9baf45399d7cc30980
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-04-29 11:02:58 +0200

    feat: added icons for use instead of htmlchars

---
+++ src/helpers/icons.ts
@@ -0,0 +1,49 @@
+
+export const historyIcon = `
+<svg width="24" height="24" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <path d="M16,30 C22.627417,30 28,24.627417 28,18 C28,11.372583 22.627417,6 16,6 C9.372583,6 4,11.372583 4,18 L4,29.5" stroke="#000000" stroke-width="3" transform="translate(16.000000, 18.000000) rotate(90.000000) translate(-16.000000, -18.000000) "></path>
+    <path d="M4.5,5.5 L7.5,2.5" stroke="#000000" stroke-width="3" stroke-linecap="square"></path>
+    <path d="M4,6 L7.5,9.5" stroke="#000000" stroke-width="3" stroke-linecap="square"></path>
+    <path d="M15.5,13.5 L15.5,18.5" stroke="#000000" stroke-width="3" stroke-linecap="square"></path>
+    <path d="M20.5,18.5 L15.5,18.5" stroke="#000000" stroke-width="3" stroke-linecap="square"></path>
+  </g>
+</svg>
+`
+
+export const trashIcon = `
+<svg width="16" height="16" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <path d="M5.5,5.5 L26.5,5.5" stroke="#000000" stroke-width="3" stroke-linecap="round"></path>
+    <path d="M25,8 C25.5522847,8 26,8.44771525 26,9 L26,28 C26,28.5522847 25.5522847,29 25,29 L7,29 C6.44771525,29 6,28.5522847 6,28 L6,9 C6,8.44771525 6.44771525,8 7,8 L25,8 Z M11,11 C10.4871642,11 10.0644928,11.3860402 10.0067277,11.8833789 L10,12 L10,25 C10,25.5522847 10.4477153,26 11,26 C11.5128358,26 11.9355072,25.6139598 11.9932723,25.1166211 L12,25 L12,12 C12,11.4477153 11.5522847,11 11,11 Z M16,11 C15.4871642,11 15.0644928,11.3860402 15.0067277,11.8833789 L15,12 L15,25 C15,25.5522847 15.4477153,26 16,26 C16.5128358,26 16.9355072,25.6139598 16.9932723,25.1166211 L17,25 L17,12 C17,11.4477153 16.5522847,11 16,11 Z M21,11 C20.4477153,11 20,11.4477153 20,12 L20,12 L20,25 C20,25.5522847 20.4477153,26 21,26 C21.5522847,26 22,25.5522847 22,25 L22,25 L22,12 C22,11.4477153 21.5522847,11 21,11 Z" fill="#000000"></path>
+  </g>
+</svg>
+`;
+
+export const trashMiniIcon = `
+<svg width="12" height="12" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <path d="M5.5,4.5 L26.5,4.5" stroke="#000000" stroke-width="5" stroke-linecap="round"></path>
+        <path d="M25,9 C26.1045695,9 27,9.8954305 27,11 L27,28 C27,29.1045695 26.1045695,30 25,30 L7,30 C5.8954305,30 5,29.1045695 5,28 L5,11 C5,9.8954305 5.8954305,9 7,9 L25,9 Z M11.5,12 C10.1745166,12 9.08996133,13.0315359 9.00531768,14.3356243 L9,14.5 L9,24.5 C9,25.8807119 10.1192881,27 11.5,27 C12.8254834,27 13.9100387,25.9684641 13.9946823,24.6643757 L14,24.5 L14,14.5 C14,13.1192881 12.8807119,12 11.5,12 Z M20.5,12 C19.1192881,12 18,13.1192881 18,14.5 L18,14.5 L18,24.5 C18,25.8807119 19.1192881,27 20.5,27 C21.8807119,27 23,25.8807119 23,24.5 L23,24.5 L23,14.5 C23,13.1192881 21.8807119,12 20.5,12 Z" fill="#000000"></path>
+    </g>
+</svg>
+`;
+
+
+export const infoIcon = `
+<svg width="16" height="16" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <path d="M16,12 L16,28" stroke="#000000" stroke-width="3" stroke-linecap="round"></path>
+    <circle fill="#000000" cx="16" cy="6" r="2"></circle>
+  </g>
+</svg>
+`;
+
+export const infoMiniIcon = `
+<svg width="12" height="12" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <path d="M16,12 L16,28" stroke="#000000" stroke-width="5" stroke-linecap="round"></path>
+    <circle fill="#000000" cx="16" cy="5" r="3"></circle>
+  </g>
+</svg>
+`;
