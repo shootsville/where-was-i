b6f3d54c4407d3c37b478645be7061b0dd934069
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-05-07 11:47:29 +0200

    0.1.13

--- src/index.ts
+++ src/index.ts
@@ -17,16 +17,16 @@ declare type WhereWasIOptions = {
   screenRefreshRate?: number
   /** adds filter to which paths should be added as location objects */
   acceptedPaths?:
-  | {
-    /** path should contain the following string */
-    type: 'contains'
-    path: string
-  }
-  | {
-    /** path should start with the following string */
-    type: 'startsWith'
-    path: string
-  }
+    | {
+        /** path should contain the following string */
+        type: 'contains'
+        path: string
+      }
+    | {
+        /** path should start with the following string */
+        type: 'startsWith'
+        path: string
+      }
 
   /** get the content of meta fields to use as metadata along each screenshot */
   metafields?: Array<string | Array<string>>
@@ -50,7 +50,7 @@ const DEFAULT_OPTIONS: WhereWasIOptions = {
   maxAmount: 12,
   style: 'cards',
   logging: 'debug',
-  zIndex: "1000"
+  zIndex: '1000',
 }
 
 let INTERVAL = 0
