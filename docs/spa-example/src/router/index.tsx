68541c1eb7afbefdc7c5c771bbae2a18a0ba2801
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-06-26 10:38:47 +0200

    feat: added tooltip to examples

--- docs/spa-example/src/router/index.tsx
+++ docs/spa-example/src/router/index.tsx
@@ -5,5 +5,15 @@ export default function Index() {
     document.body.style.background = ''
   }, [])
 
-  return <div />
+  return (
+    <div className='wwi-bubble'>
+      <div className='content'>
+        <h1>Hi!</h1>
+        <h2>
+          This is your <strong>Where Was I?</strong> button
+        </h2>
+      </div>
+      <div className='arrow'></div>
+    </div>
+  )
 }
