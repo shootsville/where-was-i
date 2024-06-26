f312e1bb1323738cb2cf746fb5823505163af66a
Gustaf Eriksson Segerdorf <gustaf@shootsville.se>
2024-06-26 10:46:06 +0200

    fix: removed erronous import

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import {
   shouldStoreNewItem,
 } from './helpers/createHistory'
 import applyCss from './helpers/applyCss'
-import 'url-change-event'
 import { Options as CanvasOptions } from 'html2canvas'
 import { logFunc, logOptions } from './helpers/logger'
 import { historyIcon } from './views/icons'
@@ -56,16 +55,16 @@ declare type WhereWasIOptions = {
   screenRefreshRate?: number
   /** adds filter to which paths should be added as location objects */
   acceptedPaths?:
-    | {
-        /** path should contain the following string */
-        type: 'contains'
-        path: string
-      }
-    | {
-        /** path should start with the following string */
-        type: 'startsWith'
-        path: string
-      }
+  | {
+    /** path should contain the following string */
+    type: 'contains'
+    path: string
+  }
+  | {
+    /** path should start with the following string */
+    type: 'startsWith'
+    path: string
+  }
   /** get the content of meta fields to use as metadata along each screenshot */
   metafields?: Array<string | Array<string>>
   /** html2canvas options, see https://html2canvas.hertzen.com/configuration for all options */
